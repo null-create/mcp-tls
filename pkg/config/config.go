623181be33a2b8f052563c3eec53520d9ee97696
jay <maestro1286@gmail.com>
2025-06-06 19:01:49 -0700

    update output

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -22,11 +22,11 @@ func LoadConfig() Config {
 	}
 	tlsKeyFile := os.Getenv("MCPTLS_KEY_FILE")
 	if tlsKeyFile == "" {
-		log.Print("WARNING MCPTLS_KEY_FILE env var not set")
+		log.Print("⚠️ WARNING MCPTLS_KEY_FILE env var not set")
 	}
 	tlsCertFile := os.Getenv("MCPTLS_CERT_FILE")
 	if tlsCertFile == "" {
-		log.Print("WARNING MCPTLS_CERT_FILE env var not set")
+		log.Print("⚠️ WARNING MCPTLS_CERT_FILE env var not set")
 	}
 
 	// check for custom server port
