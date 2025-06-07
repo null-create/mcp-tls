18e385405a88cdd582d9c036bad7fb3295c0828a
jay <maestro1286@gmail.com>
2025-06-06 17:27:30 -0700

    update main

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,30 @@
 package main
 
-import "github.com/null-create/mcp-tls/pkg/server"
+import (
+	"log"
+	"os"
+
+	"github.com/null-create/mcp-tls/pkg/server"
+)
 
 func main() {
-	server := server.NewServer()
-	server.Run()
+	router := server.NewRouter()
+
+	tlsEnabled := os.Getenv("TLS_ENABLED")
+	if tlsEnabled != "" && tlsEnabled == "true" {
+		err := server.StartSecureServer(server.TLSOptions{
+			CertFile:          "certs/server.crt",
+			KeyFile:           "certs/server.key",
+			ClientCAFile:      "certs/ca.crt", // Optional
+			RequireClientCert: false,          // Set to true if mTLS is needed
+			Addr:              ":8443",
+		}, router)
+
+		if err != nil {
+			log.Fatalf("TLS server failed: %v", err)
+		}
+	} else {
+		server := server.NewServer(router)
+		server.Run()
+	}
 }
