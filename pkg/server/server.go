0c9d4dc2ab7a8cd6b3c8a68104ef61e9d3775e7e
jay <maestro1286@gmail.com>
2025-06-09 16:29:42 -0700

    add logging to server

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -9,6 +9,9 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/google/uuid"
+	"github.com/null-create/logger"
 )
 
 type Conf struct {
@@ -32,12 +35,14 @@ func ServerConfigs() *Conf {
 type Server struct {
 	StartTime time.Time
 	Svr       *http.Server
+	log       *logger.Logger
 }
 
 func NewServer(handlers http.Handler) *Server {
 	svrCfgs := ServerConfigs()
 	return &Server{
 		StartTime: time.Now().UTC(),
+		log:       logger.NewLogger("SERVER", uuid.NewString()),
 		Svr: &http.Server{
 			Handler:      handlers,
 			Addr:         "localhost:9090",
